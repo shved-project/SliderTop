e400fe7b174bd5b053f3d1e3dd986048953b5fde
ALEX <shvitc@gmail.com>
2024-10-07 21:33:41 +0300

    Добавили useEffect на блокировку прокрутки экрана

--- src/components/PlanetSlider/index.jsx
+++ src/components/PlanetSlider/index.jsx
@@ -1,20 +1,22 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 import "./planet-slider.scss";
 
 export default function PlanetSlider() {
 	const [disableScroll, setDisableScroll] = useState(false);
 
-	if (disableScroll) {
-		const documentScroll = window.scrollY;
+	useEffect(() => {
+		if (disableScroll) {
+			const documentScroll = window.scrollY;
 
-		window.onscroll = () => {
-			window.scrollTo(0, documentScroll);
-		};
-		console.log(disableScroll);
-	} else {
-		window.onscroll = null;
-	}
+			window.onscroll = () => {
+				window.scrollTo(0, documentScroll);
+			};
+			console.log(disableScroll);
+		} else {
+			window.onscroll = null;
+		}
+	}, [disableScroll]);
 
 	return (
 		<div
